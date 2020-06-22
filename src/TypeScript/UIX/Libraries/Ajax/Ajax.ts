9b03068621c015a391910de8c31950ca6f23d824
Felix Mayer <FelixM@yer.tools>
2020-06-22 20:25:51 +0200

    Removed MIT license text from source code

--- src/TypeScript/UIX/Libraries/Ajax/Ajax.ts
+++ src/TypeScript/UIX/Libraries/Ajax/Ajax.ts
@@ -1,4 +1,3 @@
-/* Copyright (c) 2020 Felix Mayer (FelixM@yer.tools), yerTools */
 /// <reference path="IAjaxSettings.ts" />
 /// <reference path="AjaxResult.ts" />
 /// <reference path="AjaxRequest.ts" />
@@ -130,25 +129,3 @@ namespace UIX.Libraries.Ajax{
         }
     }
 }
-
-/*
- * Copyright (c) 2020 Felix Mayer (FelixM@yer.tools), yerTools
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
