8dc455e005679f622c93639512b3cf254bfbdd21
Felix Mayer <FelixM@yer.tools>
2020-07-12 22:28:16 +0200

    Updated buildtools to create UIX.Configuration.ts

---
+++ src/Utilities/Buildtools/Configuration/UIXConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buildtools.Configuration
+{
+    public class UIXConfiguration
+    {
+        public UIXConfiguration(Buildtools buildtools)
+        {
+            fileSystem.path.jsRootPath = buildtools.ServerFileStructure.JavaScriptRootPath;
+        }
+
+        public string buildTime = DateTime.UtcNow.ToString("o");
+        public FileSystem fileSystem = new FileSystem();
+
+        public class FileSystem
+        {
+            public Path path = new Path();
+            public FileName fileName = new FileName();
+
+            public class Path
+            {
+                public string jsRootPath = "/js/";
+            }
+
+            public class FileName
+            {
+                public string uix = "UIX.js";
+                public string ajaxWorker = "UIX.AjaxWorker.js";
+                public string serviceWorker = "UIX.ServiceWorker.js";
+            }
+        }
+    }
+}
