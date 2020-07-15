aadf5fba935a271704206c3939762c0ef93bc50f
Felix Mayer <FelixM@yer.tools>
2020-07-15 18:51:21 +0200

    Added Script.test() back to index

--- src/TypeScript/UIX/UIX.ts
+++ src/TypeScript/UIX/UIX.ts
@@ -6,30 +6,8 @@
 /// <reference path="Interface/AjaxInterface.ts" />
 /// <reference path="Interface/ServiceWorkerInterface.ts" />
 /// <reference path="WidgetSystem/Render.ts" />
+/// <reference path="Script/UIXScript.ts" />
 
 namespace UIX{
-    let success = false;
-    let localStoredWebsite = localStorage.getItem("webpage");
-    if(localStoredWebsite){
-        success = WidgetSystem.Render.fromJson(localStoredWebsite);
-    }
-    if(!success){
-        AjaxInterface.get("/Webpage.uix.json").then(response => {
-            if(response && response.wasSuccessfully){
-                success = WidgetSystem.Render.fromResponse(response);
-            }
-
-            if(!success){
-                AjaxInterface.get("https://uix.yer.tools/Webpage.uix.json").then(fallbackResponse => {
-                    if(fallbackResponse && fallbackResponse.wasSuccessfully){
-                        success = WidgetSystem.Render.fromResponse(fallbackResponse);
-                    }
-                    
-                    if(!success && !WidgetSystem.Render.redirectToEditMode()){
-                        WidgetSystem.Render.fallback();
-                    }
-                });
-            }
-        });
-    }
+    Script.test();
 }
