a27c4557e40b63da8e644f5cfd9240a3913b2d47
Felix Mayer <FelixM@yer.tools>
2020-07-24 23:55:57 +0200

    Added function to uninstall service wworker

--- src/TypeScript/UIX/Interface/ServiceWorkerInterface.ts
+++ src/TypeScript/UIX/Interface/ServiceWorkerInterface.ts
@@ -12,4 +12,17 @@ namespace UIX.ServiceWorkerInterface{
             console.log('Service Worker Ready');
         });
     }
+
+    export async function uninstallServiceWorkers(){
+        if(Core.Static.SharedData.Informations.features.serviceWorker){
+            let promises:Promise<boolean>[] = [];
+            let registrations = await navigator.serviceWorker.getRegistrations();
+            for(let i = 0; i < registrations.length; i++){
+                promises.push(registrations[i].unregister());
+            }
+            await Promise.all(promises);
+            return true;
+        }
+        return false;
+    }
 }
